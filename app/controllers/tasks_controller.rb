cf9dccac2d57d914b23203fe63c81af14878379d
Marcus Maione <marcusmaione@hotmail.com>
2019-02-26 17:40:09 -0300

    tasks manager application

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index         # GET /tasks
     @tasks = Task.all
   end
@@ -7,17 +8,35 @@ class TasksController < ApplicationController
   end
 
   def new           # GET /tasks/new
+    @task = Task.new
   end
 
   def create        # POST /tasks
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
   end
 
   def edit          # GET /tasks/:id/edit
   end
 
   def update        # PATCH /tasks/:id
+    @task.update(task_params)
+    redirect_to tasks_path
   end
 
   def destroy       # DELETE /tasks/:id
+    @task.destroy
+    # no need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
