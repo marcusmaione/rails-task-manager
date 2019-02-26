bfdfe6f0db749bf4c87f7cdcbe6c7c7d957201bb
Marcus Maione <marcusmaione@hotmail.com>
2019-02-26 12:07:27 -0300

    added task model

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,23 @@
+class TasksController < ApplicationController
+  def index         # GET /tasks
+    @tasks = Task.all
+  end
+
+  def show          # GET /tasks/:id
+  end
+
+  def new           # GET /tasks/new
+  end
+
+  def create        # POST /tasks
+  end
+
+  def edit          # GET /tasks/:id/edit
+  end
+
+  def update        # PATCH /tasks/:id
+  end
+
+  def destroy       # DELETE /tasks/:id
+  end
+end
